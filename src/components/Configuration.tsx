d86f7909756fbdbdbae0442af70ad98239bae104
a3104 <a3104@jaaku.org>
2025-01-28 11:07:25 +0900

    設定で全データのimport/export対応

--- src/components/Configuration.tsx
+++ src/components/Configuration.tsx
@@ -3,8 +3,13 @@ import { Pet } from '../models/pet';
 import PetFileRepository from '../repositories/PetFileRepository';
 import ClinicVisitFileRepository from '../repositories/ClinicVisitRepository';
 import { Box, Button } from "@mui/material";
+import { useState } from 'react';
+import { save } from '@tauri-apps/plugin-dialog';
+import { writeTextFile } from '@tauri-apps/plugin-fs';
 
-const Configuration = () => {
+export const Configuration = () => {
+
+    const [ setExportPetDataUrl] = useState<string>('');
 
     const handleFileUpload = async (event: React.ChangeEvent<HTMLInputElement>, importFunction: (fileContent: string) => Promise<void>) => {
         const file = event.target.files?.[0];
@@ -18,23 +23,66 @@ const Configuration = () => {
         }
     };
 
-    const importPetData = async (fileContent: string) => {
+    
+
+    const exportAllData = async () => {
+        const petRepository = new PetFileRepository();
+        const weightRepository = new WeightRepository();
+        const clinicVisitRepository = new ClinicVisitFileRepository();
+
+        const pets = await petRepository.getAllPets();
+        const weights = await weightRepository.getAllWeights();
+        const clinicVisits = await clinicVisitRepository.getAllClinicVisits();
+
+        const allData = {
+            pets: pets,
+            weights: weights,
+            clinicVisits: clinicVisits
+        };
+
+        const json = JSON.stringify(allData, null, 2);
+        // const blob = new Blob([json], { type: 'application/force-download' });
+
+
+        // const url = URL.createObjectURL(blob);
+        const filePath = await save({ defaultPath: 'all_data.json' });
+        if (filePath) {
+            await writeTextFile(filePath, json);
+           
+        }
+
+    };
+
+
+    
+    const importAllData = async (fileContent: string) => {
+        try {
+            const allData = JSON.parse(fileContent);
+            await importPetDataFromJson(allData.pets);
+            await importWeightDataFromJson(allData.weights);
+            await importClinicVisitDataFromJson(allData.clinicVisits);
+            alert('All data imported successfully!');
+        } catch (e) {
+            alert('Failed to import all data.');
+        }
+    };
+
+    const importPetDataFromJson = async (petsJson: Pet[]) => {
         const petRepository = new PetFileRepository();
         try {
-            const pets: Pet[] = JSON.parse(fileContent).map((pet: any) => {
+            const pets: Pet[] = petsJson.map((pet: any) => {
                 return Object.assign(new Pet(), pet);
             });
             await petRepository.saveAllPets(pets);
-            alert('Pet data imported successfully!');
         } catch (e) {
             alert('Failed to import pet data.');
         }
     };
 
-    const importWeightData = async (fileContent: string) => {
+    const importWeightDataFromJson = async (weightsJson: any) => {
         const weightRepository = new WeightRepository();
         try {
-            const weights = JSON.parse(fileContent); // Assuming Weight type is properly handled in WeightRepository
+            const weights = weightsJson; // Assuming Weight type is properly handled in WeightRepository
             const weightsByPetId = weights.reduce((acc: { [key: number]: any[] }, weight: { petId: number; date: string; weight: number }) => {
                 if (!acc[weight.petId]) {
                     acc[weight.petId] = [];
@@ -46,61 +94,20 @@ const Configuration = () => {
             for (const petId in weightsByPetId) {
                 await weightRepository.saveWeights(Number(petId), weightsByPetId[petId]);
             }
-            alert('Weight data imported successfully!');
         } catch (e) {
             alert('Failed to import weight data.');
         }
     };
 
-    const importClinicVisitData = async (fileContent: string) => {
+    const importClinicVisitDataFromJson = async (clinicVisitsJson: any) => {
         const clinicVisitRepository = new ClinicVisitFileRepository();
         try {
-            const clinicVisits = JSON.parse(fileContent);
-            await clinicVisitRepository.saveAllClinicVisits(clinicVisits);
-            alert('Clinic visit data imported successfully!');
+            await clinicVisitRepository.saveAllClinicVisits(clinicVisitsJson);
         } catch (e) {
             alert('Failed to import clinic visit data.');
         }
     };
 
-    const exportPetData = async () => {
-        const petRepository = new PetFileRepository();
-        const pets = await petRepository.getAllPets();
-        const json = JSON.stringify(pets, null, 2);
-        const blob = new Blob([json], { type: 'application/json' });
-        const url = URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = 'pets.json';
-        a.click();
-        URL.revokeObjectURL(url);
-    };
-
-    const exportWeightData = async () => {
-        const weightRepository = new WeightRepository();
-        const weights = await weightRepository.getAllWeights();
-        const json = JSON.stringify(weights, null, 2);
-        const blob = new Blob([json], { type: 'application/json' });
-        const url = URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = 'weights.json';
-        a.click();
-        URL.revokeObjectURL(url);
-    };
-
-    const exportClinicVisitData = async () => {
-        const clinicVisitRepository = new ClinicVisitFileRepository();
-        const clinicVisits = await clinicVisitRepository.getAllClinicVisits();
-        const json = JSON.stringify(clinicVisits, null, 2);
-        const blob = new Blob([json], { type: 'application/json' });
-        const url = URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = 'clinic_visits.json';
-        a.click();
-        URL.revokeObjectURL(url);
-    };
 
     return (
         <div>
@@ -109,35 +116,14 @@ const Configuration = () => {
             <div>
                 <Box>
                     <Button component="label" variant="contained" color="primary">
-                        ペット情報のimport
-                        <input type="file" accept=".json" onChange={(e) => handleFileUpload(e, importPetData)} hidden/>
+                        すべてのデータimport
+                        <input type="file" accept=".json" onChange={(e) => handleFileUpload(e, importAllData)} hidden />
                     </Button>
                 </Box>
+
                 <Box>
-                    <Button component="label" variant="contained" color="primary">
-                        体重情報のimport
-                        <input type="file" accept=".json" onChange={(e) => handleFileUpload(e, importWeightData)} hidden/>
-                    </Button>
-                </Box>
-                <Box>
-                    <Button component="label" variant="contained" color="primary">
-                        通院記録のimport
-                        <input type="file" accept=".json" onChange={(e) => handleFileUpload(e, importClinicVisitData)} hidden/>
-                    </Button>
-                </Box>
-                <Box>
-                    <Button variant="contained" color="primary" onClick={exportPetData}>
-                        ペット情報のexport
-                    </Button>
-                </Box>
-                <Box>
-                    <Button variant="contained" color="primary" onClick={exportWeightData}>
-                        体重情報のexport
-                    </Button>
-                </Box>
-                <Box>
-                    <Button variant="contained" color="primary" onClick={exportClinicVisitData}>
-                        通院記録のexport
+                    <Button variant="contained" color="primary" onClick={exportAllData}>
+                        すべてのデータexport
                     </Button>
                 </Box>
             </div>
@@ -145,4 +131,3 @@ const Configuration = () => {
     );
 };
 
-export default Configuration;
